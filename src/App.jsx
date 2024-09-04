09ab104766ac407ec6a5983a7642e05990e3dbbd
Andrew <andrewasnl70@gmail.com>
2024-09-04 08:03:40 +0700

    back

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from "react";
 import "./App.css";
 import data from "./data.js";
 import List from "./components/List.jsx";
-import { Route, Routes, useLocation } from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 
 import Andrew from "./components/Andrew/Andrew.jsx";
 import Kong from "./components/Kong/Kong.jsx";
@@ -23,38 +23,26 @@ import Khumphou from "./components/Khumphou/Khumphou.jsx";
 function App() {
   const [grades, setgrades] = useState(null);
 
-  function ScrollToTop() {
-    const { pathname } = useLocation(); 
-  
-    useEffect(() => {
-      
-      window.scrollTo(0, 0);
-    }, [pathname]); 
-  
-    return null; 
-  }
-
   return (
-    <div className="flex flex-col items-center gap-y-5 w-full overflow-x-hidden my-8 ">
-      <Routes>
-        <ScrollToTop/>
-        <Route path="/" element={<List students={data} grades={grades} />} />
-        <Route path="/andrew" element={<Andrew />} />
-        <Route path="/kong" element={<Kong />} />
-        <Route path="/kob" element={<Kob />} />
-        <Route path="/bm" element={<Bm />} />
-        <Route path="/boss" element={<Boss />} />
-        <Route path="/tick" element={<Tick />} />
-        <Route path="/sack" element={<Sack />} />
-        <Route path="/alex" element={<Alex />} />
-        <Route path="/peter" element={<Peter />} />
-        <Route path="/ning" element={<Ning />} />
-        <Route path="/paeng" element={<Paeng />} />
-        <Route path="/jed" element={<Jed />} />
-        <Route path="/bank" element={<Bank />} />
-        <Route path="/vidone" element={<Vidone />} />
-        <Route path="/jua" element={<Khumphou />} />
-      </Routes>
+    <div className="flex flex-col items-center gap-y-5 w-full overflow-x-hidden my-8 ">      
+        <Routes>
+          <Route path="/" element={<List students={data} grades={grades} />} />
+          <Route path="/andrew" element={<Andrew />} />
+          <Route path="/kong" element={<Kong />} />
+          <Route path="/kob" element={<Kob />} />
+          <Route path="/bm" element={<Bm />} />
+          <Route path="/boss" element={<Boss />} />
+          <Route path="/tick" element={<Tick />} />
+          <Route path="/sack" element={<Sack />} />
+          <Route path="/alex" element={<Alex />} />
+          <Route path="/peter" element={<Peter />} />
+          <Route path="/ning" element={<Ning />} />
+          <Route path="/paeng" element={<Paeng />} />
+          <Route path="/jed" element={<Jed />} />
+          <Route path="/bank" element={<Bank />} />
+          <Route path="/vidone" element={<Vidone />} />
+          <Route path="/jua" element={<Khumphou />} />
+        </Routes>
     </div>
   );
 }
